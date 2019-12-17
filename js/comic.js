f662cc10abb18fef4fc90546a2bf9f95ff304894
Erik Steinmetz <esteinmetz@gmail.com>
2019-12-16 22:05:01 -0500

    retooled js for archive page built sample archive page

--- js/comic.js
+++ js/comic.js
@@ -1,25 +1,14 @@
 /*
  *@todo:
  * -max page (dynamic)
+ * -add navbar (dynamic)
+ * -add footer (dynamic)
  */
-/* Triggers */
-window.onload = function() {
-    var last=getMaxPages();
-    var pg = parseInt(getUrlParam('p', 1));
-    if(!pg) pg=1;
-    document.getElementById('imgComic').src='res/img/' +pg+ '.gif';
-    $('#navAdvance').load('res/txt/' +pg+ '.txt');
-    $('#txtComic').load('res/txt/' +pg+ '.txt');
-    document.getElementById('navPrevious').href='index.html?p=' +(pg-1);
-    document.getElementById('navAdvance').href=document.getElementById('navNext').href='index.html?p=' +(pg<last?pg+1:pg);
-    document.getElementById('navLast').href='index.html?p=' +last;
-
-    test();
+/* Tools */
+function getMaxPages() {
+    return 2;
 }
 
-
-
-/* Tools */
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -30,14 +19,9 @@ function getUrlVars() {
 
 function getUrlParam(parameter, defaultvalue) {
     var urlparameter = defaultvalue;
-    if(window.location.href.indexOf(parameter) > -1){
+    if(window.location.href.indexOf(parameter) > -1)
         urlparameter = getUrlVars()[parameter];
-        }
     return urlparameter;
-}
-
-function getMaxPages() {
-    return 2;
 }
 
 function test() {
---
+++ js/index.js
@@ -0,0 +1,18 @@
+/* Triggers */
+window.onload = function() {
+    var last=getMaxPages();
+    var pg = parseInt(getUrlParam('p', 1));
+    if(!pg) pg=1;
+    document.getElementById('imgComic').src='res/img/' +pg+ '.gif';
+    document.getElementById('imgComic').alt='image' + pg;
+    $('#txtAdvance').load('res/txt/' +pg+ '.txt');
+    $('#txtComic').load('res/txt/' +pg+ '.txt');
+    document.getElementById('navPrevious').href='index.html?p=' +(pg-1);
+    document.getElementById('navAdvance').href=document.getElementById('navNext').href='index.html?p=' +(pg<last?pg+1:pg);
+    document.getElementById('navLast').href='index.html?p=' +last;
+
+    test();
+}
+
+
+
