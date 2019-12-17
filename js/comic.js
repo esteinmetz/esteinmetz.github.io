a3fb04e589b68c84fc7fc8fbaa1094389a90f724
Erik Steinmetz <esteinmetz@gmail.com>
2019-12-16 21:33:38 -0500

    test

--- js/comic.js
+++ js/comic.js
@@ -8,9 +8,10 @@ window.onload = function() {
     var pg = parseInt(getUrlParam('p', 1));
     if(!pg) pg=1;
     document.getElementById('imgComic').src='res/img/' +pg+ '.gif';
+    $('#navAdvance').load('res/txt/' +pg+ '.txt');
     $('#txtComic').load('res/txt/' +pg+ '.txt');
     document.getElementById('navPrevious').href='index.html?p=' +(pg-1);
-    document.getElementById('navNext').href='index.html?p=' +(pg<last?pg+1:pg);
+    document.getElementById('navAdvance').href=document.getElementById('navNext').href='index.html?p=' +(pg<last?pg+1:pg);
     document.getElementById('navLast').href='index.html?p=' +last;
 
     test();
